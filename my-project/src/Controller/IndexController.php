9c84f65579bcb6a26a00f48c5c1b2ea8f3a2dcee
mwojcikbs <mwojcikbs@gmail.com>
2022-11-02 17:32:32 +0100

    adds photo upload

--- my-project/src/Controller/IndexController.php
+++ my-project/src/Controller/IndexController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Photo;
+use App\Form\UploadPhotoType;
 use App\Repository\PhotoRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+        $form = $this->createForm(UploadPhotoType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            if($this->getUser()) {
+                $entityPhotos = new Photo();
+                $entityPhotos->setFilename($form->get('filename')->getData());
+                $entityPhotos->setIsPublic($form->get('is_public')->getData());
+                $entityPhotos->setUploadedAt(new \DateTimeImmutable());
+                $entityPhotos->setUser($this->getUser());
+
+                $em->persist($entityPhotos);
+                $em->flush();
+            }
+        }
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ my-project/src/Form/UploadPhotoType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Photo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class UploadPhotoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('filename', FileType::class, [
+                'label' => ' ',
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/*'
+                        ],
+                        'mimeTypesMessage' => 'File must be a photo'
+                    ])
+                ]
+            ])
+            ->add('is_public')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Photo::class,
+        ]);
+    }
+}
